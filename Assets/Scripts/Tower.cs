325a7eb1de1b6d269b0de3b856702524191bf1f6
Ashley Davies-Lyons <ashley@daviesl.uk>
2017-02-04 17:32:44 +0000

    Merge

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -7,6 +7,33 @@ using UnityEngine;
 class Tower : AbstractDamageTaker
 {
     private int _level;
-    private Zombie _closestZombie;
-    
+    private AbstractDamageTaker _target;
+
+    void OnCollisionStay(Collision collision)
+    {
+        AbstractDamageTaker target = collision.gameObject.GetComponent<AbstractDamageTaker>();
+
+        if (target != null && target.CompareTag("enemy") && _target == null)
+        {
+            _target = target;
+        }
+    }
+
+    void OnCollissionExit(Collision collision)
+    {
+        var target = collision.gameObject.GetComponent<AbstractDamageTaker>();
+
+        if (target == _target)
+        {
+            _target = null;
+        }
+    }
+
+    void Update()
+    {
+        if (_target != null)
+        {
+            _target.TakeDamage(1);
+        }
+    }
 }
