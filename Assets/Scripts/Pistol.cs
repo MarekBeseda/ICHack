f3699cdf7e3e56acd1826bce61a53b2ec9b7214f
Tanmay <tk915@ic.ac.uk>
2017-02-04 14:12:15 +0000

    Added basic weapon classes.

---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Gun {
+
+	// Use this for initialization
+	void Start () {
+		// Placeholder values for pistol damage and ammo.
+		damage = 0;
+		ammo = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
