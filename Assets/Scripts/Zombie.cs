f46203160695d0d302d22a3bcbecb16da77ce84d
Marek Beseda <marek.beseda16@gmail.com>
2017-02-04 16:55:15 +0000

    Player taking damage

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -5,7 +5,9 @@ public class Zombie : AbstractDamageTaker
 {
     protected NavMeshAgent nma;
     public Transform target;
-	private int power;
+	public int power;
+    public float attackStopTime;
+    private float cd;
 
     // Use this for initialization
     void Start()
@@ -16,16 +18,34 @@ public class Zombie : AbstractDamageTaker
     // Update is called once per frame
     void Update()
     {
+        if(cd > 0)
+        {
+            cd -= Time.deltaTime;
+            nma.Stop();
+        }
+        else
+        {
+            nma.Resume();
+        }
         nma.SetDestination(target.position);
     }
 
     void OnDestroy()
     {
-		
         Game.GameInstance.ZombieDied();
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        AbstractDamageTaker target = collision.gameObject.GetComponent<AbstractDamageTaker>();
+        if (target != null && cd <= 0 &&target.CompareTag("friendly"))
+        {
+            Attack(target);
+        }
+    }
+
 	void Attack(AbstractDamageTaker player) {
+        cd = attackStopTime;
 		player.TakeDamage (power);
 	}
 }
