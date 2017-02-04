733f104bc2ed1eb2c04fa692c60d2901104d65d7
geokous <george.kousouris@gmail.com>
2017-02-04 18:43:52 +0000

    Bullet kills:

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public int power;
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.GetComponent<AbstractDamageTaker>().CompareTag("enemy")) {
+			Destroy(this.gameObject);
+			collision.gameObject.GetComponent<AbstractDamageTaker> ().TakeDamage (power);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
